b93583f5ab82890e776a46c3c4f3544a3856b79d
Nicole Pingar <nicolepingar@yahoo.com>
2021-09-10 15:02:48 -0400

    final commit x2

--- script.js
+++ script.js
@@ -13,6 +13,11 @@ var numberChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 // prompt & confirm for each question/criteria
 function generatePassword() {
   var lengthConf = prompt("How many characters would you like your password to be? (8-128 characters).");
+ // if user puts an invalid number, alert will appear and then promt will appear again
+  while (lengthConf < 8 || lengthConf > 128) {
+    alert("Please enter a valid number, passwords must be between 8-128 characters.");
+    var lengthConf = prompt("How many characters would you like your password to be? (8-128 characters).");
+  }
   var lowerConf = confirm("Would you like your password to include lowercase characters?");
   var upperConf = confirm("Would you like your password to include uppercase characters?");
   var specCharConf = confirm("Would you like your password to include special characters?");
@@ -27,34 +32,34 @@ function generatePassword() {
     var numberConf = confirm("Would you like your password to include numbers?");
   }
 
-// var is empty
-var passwordCombo = []
-// if user confirms they want lowercase characters, this will add lowerChar to the empty string, passwordCombo  
-if (lowerConf = true) {
-  var passwordCombo = passwordCombo.concat(lowerChar);
-}
-// if user confirms they want uppercase characters, this will add upperChar to passwordCombo and so on  
-if (upperConf =  true) {
-  var passwordCombo = passwordCombo.concat(upperChar);
-}
-// if user clicks cancel, idiciating they don't want a certain type of characters, those characters will not be added to passwordCombo
-if (specCharConf = true) {
-  var passwordCombo = passwordCombo.concat(specialChar);
-}
-if (numberConf = true) {
-  var passwordCombo = passwordCombo.concat(numberChar);
+  // var is empty
+  var passwordCombo = []
+  // if user confirms they want lowercase characters, this will add lowerChar to the empty string, passwordCombo  
+  if (lowerConf) {
+    var passwordCombo = passwordCombo.concat(lowerChar);
+  }
+  // if user confirms they want uppercase characters, this will add upperChar to passwordCombo and so on  
+  if (upperConf) {
+    var passwordCombo = passwordCombo.concat(upperChar);
+  }
+  // if user clicks cancel, idiciating they don't want a certain type of characters, those characters will not be added to passwordCombo
+  if (specCharConf) {
+    var passwordCombo = passwordCombo.concat(specialChar);
+  }
+  if (numberConf) {
+    var passwordCombo = passwordCombo.concat(numberChar);
+  }
+
+  // empty password string 
+  var password = ""
+  // addition assignment adds chosen random characters together lengthConf amount of times 
+  for (var i = 0; i < lengthConf; i++) {
+    password = password += passwordCombo[Math.floor(Math.random() * passwordCombo.length)];
+  }
+  return password
 }
 
-// empty password string 
-var password = ""
-// addition assignment adds chosen random characters together lengthConf amount of times 
-for (var i = 0; i < lengthConf; i++) {
-password = password += passwordCombo[Math.floor(Math.random() * passwordCombo.length)];
-}
-return password
-}
-
- function writePassword() {
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
